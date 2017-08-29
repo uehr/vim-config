4fdbe6fac7938187eabed52db4bc94ed93623962
snowlt23 <shsnow23@gmail.com>
2017-08-29 22:13:41 +0900

    remove duplicate

--- cmacro.cpp
+++ cmacro.cpp
@@ -1,97 +1,85 @@
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// Preprocessor loop
-#define CAT_I(x, y) x ## y
-#define CAT(x, y) CAT_I(x, y)
+// pp basics
 
-#define INC0 1
-#define INC1 2
-#define INC2 3
-#define INC3 4
-#define INC4 5
-#define INC5 6
-#define INC6 7
-#define INC7 8
-#define INC8 9
-#define INC(i) CAT(INC, i)
+#define in ,
+#define esc_paren(...) __VA_ARGS__
+#define pp_empty(...)
+#define pp_cat_i(x, y) x ## y
+#define pp_cat(x, y) pp_cat_i(x, y)
 
-#define EMPTY(...)
-#define DEF_COMMA0 _,1 EMPTY
-#define COMMA0() ,0
-#define IS_EMPTY_III(f, s) s
-#define IS_EMPTY_II(t) IS_EMPTY_III t
-#define IS_EMPTY_I(x) IS_EMPTY_II((DEF_ ## x()))
-#define IS_EMPTY(x, ...) IS_EMPTY_I(x COMMA0)
+#define pp_inc0 1
+#define pp_inc1 2
+#define pp_inc2 3
+#define pp_inc3 4
+#define pp_inc4 5
+#define pp_inc5 6
+#define pp_inc6 7
+#define pp_inc7 8
+#define pp_inc8 9
+#define pp_inc(i) pp_cat(pp_inc, i)
 
-#define IF_0(x, y) y
-#define IF_1(x, y) x
-#define IF(cond, x, y) CAT(IF_, cond)(x, y)
+// pp args counter
 
-#define FOR_EACH_I9(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I8(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I7(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I6(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I5(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I4(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I3(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I2(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I1(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I0(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I(i, F, ...) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH(F, ...) FOR_EACH_I(0, F, __VA_ARGS__)
-#define IF_0(x, y) y
-#define IF_1(x, y) x
-#define IF(cond, x, y) CAT(IF_, cond)(x, y)
+#define def_pp_comma0 ,1
+#define pp_comma0 ,0
+#define pp_is_empty(x, ...) pp_is_empty1(x pp_comma0)
+#define pp_is_empty1(x) pp_is_empty2((def_ ## x))
+#define pp_is_empty2(t) pp_is_empty3 t
+#define pp_is_empty3(f, s) s
 
-#define FOR_EACH_I9(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I8(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I7(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I6(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I5(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I4(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I3(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I2(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I1(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I0(i, F, x, ...) F(x) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH_I(i, F, ...) IF(IS_EMPTY(__VA_ARGS__), EMPTY, CAT(FOR_EACH_I, i))(INC(i), F, __VA_ARGS__)
-#define FOR_EACH(F, ...) FOR_EACH_I(0, F, __VA_ARGS__)
-// Preprocessor loop end
+#define pp_is_one(...) pp_is_one_(__VA_ARGS__, pp_one_seq)
+#define pp_is_one_(...) pp_is_one_n(__VA_ARGS__)
+#define pp_is_one_n(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10, N, ...) N
+#define pp_one_seq 0,0,0,0,0,0,0,0,0,1,0
 
-// Preprocessor args counter
-#define PP_NARG(...) \
-         PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
-#define PP_NARG_(...) \
-         PP_ARG_N(__VA_ARGS__)
-#define PP_ARG_N( \
+#define pp_narg(...) \
+         pp_narg_(__VA_ARGS__, pp_narg_seq)
+#define pp_narg_(...) \
+         pp_arg_n(__VA_ARGS__)
+#define pp_narg_n( \
           _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
          _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
          _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
          _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
          _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
          _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
-         _61,_62,_63,N,...) N
-#define PP_RSEQ_N() \
-         63,62,61,60,                   \
+         _61,_62,_63,n,...) n
+#define pp_narg_seq \
+         63,62,61,60, \
          59,58,57,56,55,54,53,52,51,50, \
          49,48,47,46,45,44,43,42,41,40, \
          39,38,37,36,35,34,33,32,31,30, \
          29,28,27,26,25,24,23,22,21,20, \
          19,18,17,16,15,14,13,12,11,10, \
-         9,8,7,6,5,4,3,2,1,0
-#define IS_ONEARG(...) IS_ONEARG_(__VA_ARGS__,PP_RSEQ_ONE())
-#define IS_ONEARG_(...) IS_ONEARG_N(__VA_ARGS__)
-#define IS_ONEARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
-#define PP_RSEQ_ONE() 0,0,0,0,0,0,0,0,0,1,0
+          9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
-// Preprocessor args counter end
+// pp if
+#define pp_if_0(x, y) y
+#define pp_if_1(x, y) x
+#define pp_if(cond, x, y) pp_cat(pp_if_, cond)(x, y)
+
+// pp foreach
+#define pp_foreach_i9(i, f, x, ...) f(x) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach_i8(i, f, x, ...) f(x) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach_i7(i, f, x, ...) f(x) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach_i6(i, f, x, ...) f(x) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach_i5(i, f, x, ...) f(x) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach_i4(i, f, x, ...) f(x) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach_i3(i, f, x, ...) f(x) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach_i2(i, f, x, ...) f(x) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach_i1(i, f, x, ...) f(x) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach_i0(i, f, x, ...) f(x) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach_i(i, f, ...) pp_if(pp_is_empty(__VA_ARGS__), pp_empty, pp_cat(pp_foreach_i, i))(pp_inc(i), f, __VA_ARGS__)
+#define pp_foreach(f, ...) pp_foreach_i(0, f, __VA_ARGS__)
 
 // alternative rep macro
-#define esc_paren(...) __VA_ARGS__
-#define in ,
 #define rep(...) rep1(rep2, (__VA_ARGS__))
 #define rep1(X, A) X A
-#define rep2(i, ...) IF(IS_ONEARG(__VA_ARGS__), rep_len1(i, __VA_ARGS__), rep3(i, __VA_ARGS__))
+#define rep2(i, ...) pp_if(pp_is_one(__VA_ARGS__), rep_len1(i, __VA_ARGS__), rep3(i, __VA_ARGS__))
 #define rep_len1(i, end, ...) for (int i = 0; i < end; i++)
 #define rep3(i, iter, ...) rep4(rep5, (iter, i, esc_paren __VA_ARGS__))
 #define rep4(X, A) X A
@@ -110,6 +98,16 @@ using namespace std;
 #define viter viter_iter ,
 #define viter_iter(i, v) auto tmpitr=v.begin(); for (auto i=*tmpitr; tmpitr != v.end(); tmpitr++)
 
+// debug macros
+#define debug_var_elem(v) << #v << "=" << v << " "
+#ifdef DEBUG
+#define debug_echo(e) cout << "L" << __LINE__ << ": " << e << endl
+#define debug_var(...) cout << "L" << __LINE__ << ": " pp_foreach(debug_var_elem, __VA_ARGS__) << endl
+#else
+#define debug_echo(e)
+#define debug_var(...)
+#endif
+
 // short names
 typedef long long ll;
 typedef string str;
@@ -127,23 +125,19 @@ ll lcm(ll a,ll b){return a / gcd(a,b) * b;}
 #define out(x) for(auto i : x) cout << i << " "; cout << endl;
 #define outln(x) for(auto i : x) cout << i << " " << endl;
 #define vec_cpy(to,from) copy(all(to),back_inserter(from))
-#define ary_cpy(to,from) memcpy(to, from, sizeof from)
+#define ary_cpy(to,from) memcpy(to, from, sizeof(from))
 #define MOD = 1000000007; 
-
-// debug macros
-#define DEBUG_VAR(v) << #v << "=" << v << " "
-#ifdef DEBUG
-#define debug_echo(e) cout << "L" << __LINE__ << ": " << e << endl
-#define debug_var(...) cout << "L" << __LINE__ << ": " FOR_EACH(DEBUG_VAR, __VA_ARGS__) << endl
-#define debug_echo_dp1(dp, N) { cout << "[ "; rep(i, N) cout << dp[i] << " ";  cout << "]" << endl; }
-#else
-#define debug_echo(e)
-#define debug_var(...)
-#define debug_echo_dp1(dp, N)
-#endif
 
 // Implementation
 
 int main() {
+  pp_is_empty(0, 0, 0, 0, 0);
+  pp_is_empty(0);
+  pp_is_empty();
+  rep (i in 5) {
+    rep (j in 5) {
+      debug_var(i, j);
+    }
+  }
 }
 
